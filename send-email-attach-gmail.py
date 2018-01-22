3f69285b210923bdf2610d0726eaa3b7aa88ddaa
Gagandeep Singh <gagands3@cisco.com>
2018-01-23 00:06:37 +0530

    script to send email with attachment using gmail account - password embedded

---
+++ send-email-attach-gmail.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
+
+
+recipients = ['rcpt1@example.com','rcpt2@example.com','group1@example.com']
+emaillist = [elem.strip().split(',') for elem in recipients]
+msg = MIMEMultipart()
+msg['Subject'] = str(sys.argv[1])
+msg['From'] = 'abcxyz@gmail.com'
+msg['Reply-to'] = 'abcxyz@gmail.com'
+ 
+msg.preamble = 'Multipart massage.\n'
+ 
+part = MIMEText("Hi, please find the attached file")
+msg.attach(part)
+ 
+part = MIMEApplication(open(str(sys.argv[2]),"rb").read())
+part.add_header('Content-Disposition', 'attachment', filename=str(sys.argv[2]))
+msg.attach(part)
+ 
+
+server = smtplib.SMTP("smtp.gmail.com:587")
+server.ehlo()
+server.starttls()
+server.login("abcxyz@gmail.com", "yourpasswordhere")
+ 
+server.sendmail(msg['From'], emaillist , msg.as_string())
